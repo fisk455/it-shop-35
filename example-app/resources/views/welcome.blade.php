a50b39e6a1054d7c497eef383aab256cd313871d
fisk455 <fisk455@gmail.com>
2022-07-18 22:54:32 -0700

    Update welcome.blade.php

--- example-app/resources/views/welcome.blade.php
+++ example-app/resources/views/welcome.blade.php
@@ -22,12 +22,11 @@
     </head>
     <body class="antialiased">
         <div>
-        <h1 style="text-align: center;"></h1>
-            <h1>ภวัต ศรทัตต์ เลขที่ 21</h1>
-            <h1>
+        <h1 style="text-align: center;">
+                ภวัต ศรทัตต์ เลขที่ 21
+            <br>
                 {{date('D:M:Y H:i:s')}}
-            </h1>
-        </div>
+        </h1>
         <!-- <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
